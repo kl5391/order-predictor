2e1b1d54616ba2a495579728433fa96f92820683
Kyle Long <kl5391@rit.edu>
2025-11-20 13:21:41 -0500

    initial logic for destructuring, objectification, and grouped output of entry objects

--- API/src/prediction_algorithim/parser.py
+++ API/src/prediction_algorithim/parser.py
@@ -1,13 +1,30 @@
 import csv;
 
+class EntryObject:
+    def __init__(self, Ticker, Type, Quantity, Price, Amount):
+          self.Ticker = Ticker
+          self.Type = Type
+          self.Quantity = Quantity
+          self.Price = Price
+          self.Amount = Amount
+    
+
 def getEntryObjects(path = None):
+    parsedTrades = []
     if path == None:
             path = input('Input file path to parse\nPath: ')
     try:
         with open(path, newline='') as csvfile:
-            reader = csv.DictReader(csvfile)
-            for row in reader:
-                    print(row)
+            reader = csv.reader(csvfile)
+            header = next(reader)
+            indexes = {"ticker": header.index('Instrument'), "type": header.index('Trans_Code'), 
+                            "quantity": header.index('Quantity'), "price": header.index('Price'), "amount": header.index('Amount')}
+            for entry in reader:
+                  parsedTrades.append(EntryObject(entry[indexes['ticker']], entry[indexes['type']], entry[indexes['amount']], entry[indexes['price']], entry[indexes['amount']]))
+            return parsedTrades
+                    
+                          
+                    
     except FileNotFoundError:
           print('A file with this path could not be found')
 
