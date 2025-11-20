8f603070e4f9d1af77b0d507119ade1de2c04b97
Kyle Long <kl5391@rit.edu>
2025-11-20 15:57:34 -0500

    refactoring for maintainability

--- API/src/prediction_algorithim/parser.py
+++ API/src/prediction_algorithim/parser.py
@@ -1,39 +1,60 @@
-import csv;
+import csv
+
+# For easy testing
+test_path = "/Users/kylelong/Downloads/RH.csv"
+
 
 class EntryObject:
-      def __init__(self, Ticker, Type, Quantity, Price, Amount):
-          self.Ticker = Ticker
-          self.Type = Type
-          self.Quantity = Quantity
-          self.Price = Price
-          self.Amount = Amount
+    def __init__(self, Ticker, Type, Quantity, Price, Amount):
+        if Type == "ACH":
+            raise TypeError("Unspported type will not impact model decisions")
+        self.Ticker = Ticker
+        self.Type = Type
+        self.Quantity = Quantity
+        self.Price = Price
+        self.Amount = Amount
 
-      def __str__(self):
-           return f"Ticker: {self.Ticker}\nOrder Type: {self.Type}\nOrder Quantity:{self.Quantity}\nOrder Price:{self.Price}Order Amount:{self.Amount}"
-           
-    
+    def __str__(self):
+        return f"Ticker: {self.Ticker} Order Type: {self.Type} Order Quantity: {self.Quantity} Order Price: {self.Price}Order Amount: {self.Amount}"
 
-def getEntryObjects(path = None):
+
+def getEntryObjects(path=None):
     parsedTrades = []
     if path == None:
-            path = input('Input file path to parse\nPath: ')
+        path = input("Input file path to parse\nPath: ")
     try:
-        with open(path, newline='') as csvfile:
+        with open(path, newline="") as csvfile:
             reader = csv.reader(csvfile)
             header = next(reader)
-            indexes = [header.index('Instrument'), header.index('Trans Code'), 
-                            header.index('Quantity'), header.index('Price'), header.index('Amount')]
+            indexes = {
+                "ticker": header.index("Instrument"),
+                "type": header.index("Trans Code"),
+                "quantity": header.index("Quantity"),
+                "price": header.index("Price"),
+                "amount": header.index("Amount"),
+            }
             for entry in reader:
-                  try:
-                        parsedTrades.append(EntryObject(entry[indexes[0]], entry[indexes[1]], entry[indexes[2]], entry[indexes[3]], entry[indexes[4]]))
-                  except IndexError:
-                        continue
+                try:
+                    parsedTrades.append(
+                        EntryObject(
+                            entry[indexes['ticker']],
+                            entry[indexes['type']],
+                            entry[indexes['quantity']],
+                            entry[indexes['price']],
+                            entry[indexes['amount']],
+                        )
+                    )
+                except TypeError:
+                    continue
+                except IndexError:
+                    continue
             return parsedTrades
     except FileNotFoundError:
-          print('A file with this path could not be found')
+        print("A file with this path could not be found")
 
-if __name__ == '__main__':
-      response = getEntryObjects()
-      if (response):
-           for entry in response:
-                print(str(entry) + "\nNEXT ORDER")
+
+if __name__ == "__main__":
+    response = getEntryObjects(test_path)
+    if response:
+        for entry in response:
+            print(str(entry))
