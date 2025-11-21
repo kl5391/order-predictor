3364c53f1a6b1d963fac2cdf23d55474fccfcfa3
Kyle Long <kl5391@rit.edu>
2025-11-21 00:33:18 -0500

    prediction API almost done

--- API/src/prediction_algorithim/prediction_api.py
+++ API/src/prediction_algorithim/prediction_api.py
@@ -1,6 +1,6 @@
 from google import genai
 from google.genai import types
-from parser import getEntryObjects,getEntryTickers
+from parser import getEntryObjects, getEntryTickers
 from pydantic import BaseModel, Field
 from stock_data import getTickerData
 
@@ -9,10 +9,19 @@ client = genai.Client()
 
 # For easy testing
 test_path = "/Users/kylelong/Downloads/RH.csv"
+initial_system_instruction = "You are to recommend new stock tickers based on the submitted users trading patterns"
 
 
-class TickerSet(BaseModel):
+class InitialTickerSet(BaseModel):
     tickers: list[str] = Field(description="list of stock tickers. minimum of 8")
+
+"""class ValidatedStock(BaseModel):
+    ticker: str = Field(description="name of ticker")
+    reason: str = Field(description="brief reason for why stock is a good fit")
+    data: str = Field(description="relevant data for selected stock. don't be afraid to use actual numbers")"""
+
+class ReturnedDataSet(BaseModel):
+    stocks: list[str] = Field(description="List of validated stocks with reasons for why each one was picked and relevant data for each")
 
 
 def userAnalysis(path):
@@ -24,22 +33,38 @@ def userAnalysis(path):
         model="gemini-2.5-flash",
         contents=entryString,
         config=types.GenerateContentConfig(
-            system_instruction="You are to recommend new stock tickers based on the submitted users trading patterns",
+            system_instruction=initial_system_instruction,
             response_mime_type="application/json",
-            response_json_schema=TickerSet.model_json_schema(),
+            response_json_schema=InitialTickerSet.model_json_schema(),
         ),
     )
-    tickerSet = TickerSet.model_validate_json(response.text)
-    
-    tickerExamanation(tickerSet.tickers, getEntryTickers(path))
+    tickerSet = InitialTickerSet.model_validate_json(response.text)
+    return tickerSet.tickers
 
 
 def tickerExamanation(suggestedTickers, userTickers):
-    userData = getTickerData(userTickers)
-    suggestedData = getTickerData(suggestedTickers)
-    print(suggestedData)
+    userData = str(getTickerData(userTickers))
+    suggestedData = str(getTickerData(suggestedTickers))
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=[
+            userData,
+            "THIS IS THE SPLIT. THIS IS THE BEGINNING OF THE PROPOSED DATA",
+            suggestedData,
+        ],
+        config=types.GenerateContentConfig(
+            system_instruction=f"You need to determine if these stocks are comparable based on your initial prompt. Do not be afraid to disinclude tickers that do not match {initial_system_instruction}",
+            response_mime_type="application/json",
+            response_json_schema=ReturnedDataSet.model_json_schema(),
+        ),
+    )
+    validatedTickerSet = ReturnedDataSet.model_validate_json(response.text)
+    return validatedTickerSet
 
 
 if __name__ == "__main__":
     userPath = getEntryObjects(test_path)
     callResponse = userAnalysis(test_path)
+    result = tickerExamanation(callResponse, getEntryTickers(test_path))
+    for stock in result.stocks:
+        print(stock)
