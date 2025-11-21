08b20ec0cecdd943f52cfe82276b6103c1c1f3b5
Kyle Long <kl5391@rit.edu>
2025-11-20 21:57:55 -0500

    model_api switchover

--- API/src/prediction_algorithim/model_api.py
+++ API/src/prediction_algorithim/model_api.py
@@ -1,37 +1,38 @@
 from openai import OpenAI
 from parser import getEntryObjects
 from pydantic import BaseModel
+
 client = OpenAI()
 
 
 # For easy testing
 test_path = "/Users/kylelong/Downloads/RH.csv"
 
+
 class TickerSet(BaseModel):
     tickers: list[str]
 
+
 def userAnalysis(path):
     entryObjects = getEntryObjects(path)
-    entryString = ''
+    entryString = ""
     for entry in entryObjects:
-        entryString += f'{entry}, '
-    model_response = client.responses.create(
+        entryString += f"{entry}, "
+    model_response = client.responses.parse(
         model="gpt-3.5-turbo",
         tools=[{"type": "web_search"}],
-        input = [
+        input=[
             {
-              "role": "user",
-              "content": entryString
-            },
-            {
-                "role": "devloper",
+                "role": "system",
                 "content": "these are trades submitted by a user. you need to determine a list of accetable tickers to suggest to the user based on the trades they "
-                "have provided you"
-            }
+                "have provided you",
+            },
+            {"role": "user", "content": entryString},
         ],
-        text_format=TickerSet
+        text_format=TickerSet,
     )
+    return model_response.output_parsed
+
 
 if __name__ == "__main__":
     print(userAnalysis(test_path))
-    
