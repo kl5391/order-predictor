3b725cf84bf4d3a35d3cb2839e8f40cbd5fe9ece
Kyle Long <kl5391@rit.edu>
2025-11-20 23:28:43 -0500

    fire api

--- API/src/prediction_algorithim/prediction_api.py
+++ API/src/prediction_algorithim/prediction_api.py
@@ -2,6 +2,7 @@ from google import genai
 from google.genai import types
 from parser import getEntryObjects
 from pydantic import BaseModel, Field
+from stock_data import getTickerData
 
 client = genai.Client()
 
@@ -11,7 +12,7 @@ test_path = "/Users/kylelong/Downloads/RH.csv"
 
 
 class TickerSet(BaseModel):
-    tickers: list[str] = Field(description="list of stock tickers")
+    tickers: list[str] = Field(description="list of stock tickers. minimum of 8")
 
 
 def userAnalysis(path):
@@ -23,14 +24,21 @@ def userAnalysis(path):
         model="gemini-2.5-flash",
         contents=entryString,
         config=types.GenerateContentConfig(
-        system_instruction="You are to recommend new stock tickers based on the submitted users trading patterns", response_mime_type="application/json", 
-        response_json_schema=TickerSet.model_json_schema()),
+            system_instruction="You are to recommend new stock tickers based on the submitted users trading patterns",
+            response_mime_type="application/json",
+            response_json_schema=TickerSet.model_json_schema(),
+        ),
     )
     tickerSet = TickerSet.model_validate_json(response.text)
-    return tickerSet
+    return tickerSet.tickers
+
+def tickerExamanation(tickers):
+    #implement function call that determines 
+    return None
 
 
 if __name__ == "__main__":
     callResponse = userAnalysis(test_path)
-    for result in callResponse:
-        print(result)
+    data = getTickerData(callResponse)
+    for row in data:
+        print(row)
---
+++ API/src/prediction_algorithim/stock_data.py
@@ -0,0 +1,9 @@
+import yfinance as yf
+
+def getTickerData(tickers):
+    ticker_array = []
+    for ticker in tickers:
+        data = yf.Ticker(ticker)
+        data.history(period='1mo')
+        ticker_array.append(data.info)
+    return ticker_array
