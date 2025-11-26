387f6078b55ac665b312411ae1cb34fe74ad60e4
Kyle Long <kl5391@rit.edu>
2025-11-26 10:27:16 -0500

    uploading works

--- API/src/prediction_algorithim/server.py
+++ API/src/prediction_algorithim/server.py
@@ -3,6 +3,7 @@ from flask_restful import Resource, Api, reqparse
 from prediction_api import userAnalysis, tickerExamanation
 from parser import getEntryObjects
 from stock_data import getTickerData
+from werkzeug.datastructures import FileStorage
 
 app = Flask(__name__)
 api = Api(app)
@@ -11,9 +12,23 @@ api = Api(app)
 class TickerData(Resource):
     def get(self, ticker):
         return {"data": getTickerData(ticker)}
+    
+class Upload(Resource):
+    def get(self):
+        return({"data": "youre at the right endpoint"})
+
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('file', location='files', type=FileStorage, required=True)
+        args = parser.parse_args()
+        csv_file: FileStorage = args['file']
+        if csv_file.filename.endswith(".csv"):
+            return{"file_name": csv_file.filename}
+        
 
 
 api.add_resource(TickerData, "/info/<ticker>")
+api.add_resource(Upload, "/upload")
 
 if __name__ == "__main__":
     app.run(debug=True)
