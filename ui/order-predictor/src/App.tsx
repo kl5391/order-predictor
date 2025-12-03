ae465ccf96f4d28ab215f1d62da80f38bb01b938
Kyle Long <kl5391@rit.edu>
2025-12-03 09:50:46 -0500

    need to figure out why some components are being troublesome

--- ui/order-predictor/src/App.tsx
+++ ui/order-predictor/src/App.tsx
@@ -1,10 +1,16 @@
 import { useState } from "react";
 import "./App.css";
+import { Button } from "./components/ui/button";
+import { Orbit } from "lucide-react";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <></>;
+  return (
+    <>
+      <Orbit />
+    </>
+  );
 }
 
 export default App;
