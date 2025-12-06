f4fd98a8c53bda56cfb295919040aaac665ebdf1
Kyle Long <kl5391@rit.edu>
2025-12-06 14:28:58 -0500

    just needs a more details button

--- ui/order-predictor/src/GeneratedPage.jsx
+++ ui/order-predictor/src/GeneratedPage.jsx
@@ -6,20 +6,26 @@ function GeneratedPage({ analyzedTickers }) {
     return (
       <div className="analyzed-stock">
         <h1>Stock name: {stock.ticker}</h1>
-        <p>{stock.reason}</p>
-        <p>{stock.data}</p>
+        <div>
+          <b>Why this was chosen for you</b>
+          <p>{stock.reason}</p>
+        </div>
+        <div>
+          <b>What the data says</b>
+          <p>{stock.data}</p>
+        </div>
       </div>
     );
   };
 
   return (
-    <>
+    <div className="generated-page">
       <Carousel
         value={analyzedTickers}
         numScroll={3}
         itemTemplate={stockTemplate}
       />
-    </>
+    </div>
   );
 }
 export default GeneratedPage;
