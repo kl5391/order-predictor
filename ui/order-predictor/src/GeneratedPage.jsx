ebbf302aeaf897a4ba350c68cb29533cae983f01
Kyle Long <kl5391@rit.edu>
2025-12-06 14:24:47 -0500

    working analysis page

--- ui/order-predictor/src/GeneratedPage.jsx
+++ ui/order-predictor/src/GeneratedPage.jsx
@@ -1,7 +1,24 @@
+import { Carousel } from "primereact/carousel";
+
 function GeneratedPage({ analyzedTickers }) {
+  const stockTemplate = (stock) => {
+    console.log(stock);
+    return (
+      <div className="analyzed-stock">
+        <h1>Stock name: {stock.ticker}</h1>
+        <p>{stock.reason}</p>
+        <p>{stock.data}</p>
+      </div>
+    );
+  };
+
   return (
     <>
-      <h1>hey</h1>
+      <Carousel
+        value={analyzedTickers}
+        numScroll={3}
+        itemTemplate={stockTemplate}
+      />
     </>
   );
 }
