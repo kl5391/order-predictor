966f8b9baa2e0fd50ef349148a7525adc1d296ba
Kyle Long <kl5391@rit.edu>
2025-12-13 15:22:25 -0500

    working query and useeefect

--- ui/order-predictor/src/InfoCard.jsx
+++ ui/order-predictor/src/InfoCard.jsx
@@ -1,28 +1,30 @@
 import { OverlayPanel } from "primereact/overlaypanel";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function InfoCard({ ticker }) {
   const [tickerInfo, setTickerInfo] = useState({});
 
-  const getTickerInfo = async (ticker) => {
-    try {
-      const response = await fetch(`http://127.0.0.1:5000/ticker/${ticker}`, {
-        method: "GET",
-        headers: { "Content-Type": "Application/json" },
-      });
+  useEffect(() => {
+    async function setData() {
+      try {
+        const response = await fetch(`http://127.0.0.1:5000/info/${ticker}`, {
+          method: "GET",
+          headers: { "Content-Type": "text/plain" },
+        });
 
-      if (response.ok) {
-        const result = await response.json();
-        const stockData = result["data"];
-        setTickerInfo(stockData);
-        console.log(stockData);
-      } else {
-        console.error("Could not process user data", response.statusText);
+        if (response.ok) {
+          const result = await response.json();
+          const stockData = result["data"];
+          setTickerInfo(stockData);
+        } else {
+          console.error("Could not process user data", response.statusText);
+        }
+      } catch (error) {
+        console.error("Error during ticker analysis:", error);
       }
-    } catch (error) {
-      console.error("Error during ticker analysis:", error);
     }
-  };
+    setData();
+  }, [ticker]);
 
   return <h1>{tickerInfo["displayName"]}</h1>;
 }
